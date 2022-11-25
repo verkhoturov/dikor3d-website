0df58fa91bd0512b21d438bd2d0705813856e60a
VerkhoturovIP <verkhoturovip@sibur.ru>
2022-11-25 13:33:51 +0300

    fix manifest

--- components/common/meta.tsx
+++ components/common/meta.tsx
@@ -36,7 +36,7 @@ export const Meta = ({
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
